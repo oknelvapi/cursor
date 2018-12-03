6535a678a7c8d38b3f7b6aeac53fd01ceb077a0e
oknelvapi <eldhim@gmail.com>
2018-12-03 12:00:29 +0200

    add delete & add

--- L6W6_task_01/js/script2.js
+++ L6W6_task_01/js/script2.js
@@ -8,68 +8,129 @@ const btnXmlh = document.querySelector("#btn-xmlh"),
       cardBox = document.getElementsByClassName("main__cardBox"),
       swipe = document.querySelector(".button__swipe");
 
+
 //  |---------------------------|
 //  |    Users cards creator    |
 //  |---------------------------|
-
 const renderUsers = users => {
     const cardBox = document.querySelector("#card-box")
     cardBox.innerHTML = "";
     _.each(users['data'], value => {
-        const userField = document.createElement('div'),
-              userImg = document.createElement('img'),
-              cardBody = document.createElement('div'),
-              userName = document.createElement('h5'),
-              userAge = document.createElement('p'),
-              btnEdit = document.createElement('button');
-              btnDel = document.createElement('button');
-
-        userName.textContent = 'Name: '+value.name;
-        userAge.textContent = 'Age: '+value.age;
-        
-        userField.className = "card";
-        userImg.className = "card-img-top";
-        cardBody.className = "card-body";
-        userName.className = "card-title";
-        btnEdit.className = "btn btn-primary"
-        btnDel.className = "btn del btn-primary" 
-
-        userImg.setAttribute('src', 'img/man.svg');
-        userImg.setAttribute('alt', 'User image');
-        btnEdit.innerText = 'Edit';
-        btnDel.innerText = 'Delete';
-
-        cardBox.prepend(userField);
-        
-        
-        userField.appendChild(userImg);
-        userField.appendChild(cardBody);
-        cardBody.appendChild(userName);
-        cardBody.appendChild(userAge);
-        cardBody.appendChild(btnEdit);
-        cardBody.appendChild(btnDel);
-
+        renderOneUser(value);
     })
 }
 
+//  |---------------------------|
+//  |  Singl user card creator  |
+//  |---------------------------|
 
+const renderOneUser = (value) => {
+    const cardBox = document.querySelector("#card-box")
+    const userField = document.createElement('div'),
+          userImg = document.createElement('img'),
+          cardBody = document.createElement('div'),
+          userName = document.createElement('h5'),
+          userAge = document.createElement('p'),
+          btnEdit = document.createElement('button');
+          btnDel = document.createElement('button');
+    
+    
+
+    userName.textContent = `Name: ${value.name}`;
+    userAge.textContent = `Age: ${value.age}`;
+        
+    userField.className = "card";
+    userImg.className = "card-img-top";
+    cardBody.className = "card-body";
+    userName.className = "card-title";
+    btnEdit.className = "btn btn-primary"
+    btnDel.className = "btn del btn-primary" 
+
+    userImg.setAttribute('src', 'img/man.svg');
+    userImg.setAttribute('alt', 'User image');
+    btnEdit.innerText = 'Edit';
+    btnDel.innerText = 'Delete';
+
+    cardBox.prepend(userField);
+        
+        
+    userField.appendChild(userImg);
+    userField.appendChild(cardBody);
+    cardBody.appendChild(userName);
+    cardBody.appendChild(userAge);
+    cardBody.appendChild(btnEdit);
+    cardBody.appendChild(btnDel);
+debugger
+    btnDel.addEventListener('click', delUser.bind(userField, value.id));
+}
+
+//  |---------------------------|
+//  |      Add a new user       |
+//  |---------------------------|
+// const addUser = document.querySelector("#add-User");
+// addUser.addEventListener("click", () => {
+//     const uname = document.querySelector("#modal-name").value,
+//           uage = document.querySelector("#modal-age").value;
+//     fetch('https://test-users-api.herokuapp.com/users', {
+//         method: 'POST',
+//         body: JSON.stringify({ name: uname, age: uage}),
+//         headers: {
+//             Accept: 'application/json',
+//             'Content-Type': 'application/json',
+//         }
+//     });
+// })
+const addUser  = async() => {
+    try {
+        const uname = document.querySelector("#modal-name").value,
+              uage = document.querySelector("#modal-age").value;
+              debugger
+        const userResponse = await request('POST', {
+            name: uname,
+            age: uage
+        })     
+        renderOneUser(userResponse);
+    } catch(err) {
+        alert(err);
+    }
+}
+
+document.querySelector("#add-User").addEventListener("click", addUser);
+
+//  |---------------------------|
+//  |    Delete a singl user    |
+//  |---------------------------|
+const delUser = async function(id){
+    try {
+        const userResponse = await request(`${id}`, "DELETE");
+        debugger
+        this.remove();
+    } catch(err) {
+        alert(err);
+    }
+    
+
+}
+
+//  |---------------------------|
+//  |  Modal windows settings   |
+//  |---------------------------|
 userCreate.addEventListener('click', () =>{
-    const modal = document.querySelector(".modal-content");
+    const modal = document.querySelector("#modalBox");
     modal.style.width = "15vw";
 })
+
 //  |---------------------------|
-//  |     Delete the cards      |
+//  |     Remove all cards      |
 //  |---------------------------|
 swipe.addEventListener('click', () =>{
     const cardBox = document.querySelector("#card-box")
     cardBox.innerHTML = ""; 
 })
-
-      
+   
 //  |---------------------------|
 //  |  XMLHttpRequest method    |
 //  |---------------------------|
-
 btnXmlh.addEventListener('click', () => {
     //cardBox.innerHTML = "";
     const request = new XMLHttpRequest();
@@ -88,14 +149,10 @@ btnXmlh.addEventListener('click', () => {
 //  |---------------------------|
 //  |         Fetch API         |
 //  |---------------------------|
-
 btnAsync.addEventListener('click', async () => {
     try {
-        debugger
         const users = await request();
-        
         renderUsers(users);
-            //setSinglContent(users)
     } catch (err) {
         alert(`Get Users: ${err}`);
     }
@@ -120,15 +177,15 @@ const request = (method = "GET", data = {}) => {
 
 
 
+//  |---------------------------|
+//  |      Bootstrap modal      |
+//  |---------------------------|
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
-
-
 //  |---------------------------|
 //  |      Bootstrap tooltip    |
 //  |---------------------------|
-
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
